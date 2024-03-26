42de0c5e913d0d2f0db41c9e686eebfd4da2ab2d
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-03-26 11:15:36 +0900

    Update [WIP] Matrix.cpp

--- Math/[WIP] Matrix.cpp
+++ Math/[WIP] Matrix.cpp
@@ -1,17 +1,14 @@
 using matrix = vector<vector<int>>;
 matrix fail = {{-1}};
-const int MOD = 1e9+7;
-template<typename T>
-void init(T&A,int r,int c){
+void init(matrix&A,int r,int c){
 	A.resize(r);
 	for(int i=0;i<r;i++) A[i].resize(c);
 }
-template<typename T>
-T operator*(T A,T B){
+const int MOD = 1e9+7;
+matrix operator*(const matrix&A,const matrix&B){
 	int r1=A.size(),c1=A[0].size(),r2=B.size(),c2=B[0].size();
 	if(c1!=r2) return fail;
 	T C; init(C,r1,c2);
-	for(int i=0;i<r1;i++) C[i].resize(c2);
 	for(int i=0;i<r1;i++)
 		for(int k=0;k<c1;k++){
 			const ll tmp = A[i][k];
@@ -22,16 +19,12 @@ T operator*(T A,T B){
 		}
 	return C;
 }
-
-template<typename T1,typename T2>
-T1 operator^(T1 A,T2 x){
-	if(x==0) return fail;
+matrix operator^(const matrix&A,ll x){
+	if(x<=0) return fail;
 	T1 ans = A; --x;
 	while(x>0){
-		if(x&1)
-			ans = ans * A;
-		x>>=1;
-		A = A*A;
+		if(x&1) ans = ans * A;
+		x>>=1; A = A*A;
 	}
 	return ans;
 }
