eee9e822da1b117dbede8a93204c3e3546ec8fe6
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-03-26 10:59:42 +0900

    Create ext_gcd&inv.cpp

---
+++ Math/ext_gcd&inv.cpp
@@ -0,0 +1,16 @@
+ll ext_gcd(ll a, ll b, ll& x, ll& y) {
+    assert(a>=0 && b>=0); // it actually works when a<0 or b<0
+    ll g = a;
+    x = 1; y = 0;
+    // note that x and y are swapped
+    if(b != 0) g = ext_gcd(b, a%b, y, x), y -= a/b*x; 
+    return g;
+}
+
+ll inv(ll a, ll m){
+    // return x that a * x = 1 mod m
+    ll x, y; 
+    ll g = ext_gcd(a, m, x, y);
+    if(g != 1) return -1;
+    return (x%m + m) % m;
+}
