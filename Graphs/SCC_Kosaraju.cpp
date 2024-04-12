640899cb10ad1e7c0fdc46b46c4f0a19dbaf35d1
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-04-12 16:03:54 +0900

    Create SCC_Kosaraju.cpp

---
+++ Graphs/SCC_Kosaraju.cpp
@@ -0,0 +1,30 @@
+/*
+Find Stronly Connected Components
+Kosaraju's algorithm
+O(V+E)
+*/
+namespace SCC{
+    vector<int> g[N], gR[N], S;
+    vector<bool> vis;
+    int scc[N], scnt;
+    void add_edge(int a,int b){
+        g[a].push_back(b);
+        gR[b].push_back(a);
+    }
+    void dfs(int v, bool rev = false){
+        vis[v] = true;
+        auto&G = !rev ? g: gR;
+        if(rev) scc[v] = scnt;
+        for(auto nxt:G[v])
+            if(!vis[nxt])
+                dfs(nxt, rev);
+        if(!rev) S.push_back(v);
+    }
+    void get_SCC(int n){
+        vis.assign(n+1, false);
+        for(int i=1;i<=n;i++) if(!vis[i]) dfs(i, false);
+        vis.assign(n+1, false);
+        reverse(all(S));
+        for(int x: S) if(!vis[x]) ++scnt, dfs(x, true);
+    }
+} using namespace SCC;
