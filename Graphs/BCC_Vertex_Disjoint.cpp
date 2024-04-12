7d25ce25af719652db54797bf724f35c40a9ddc5
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-04-12 20:19:09 +0900

    Update BCC_Vertex_Disjoint.cpp

--- Graphs/BCC_Vertex_Disjoint.cpp
+++ Graphs/BCC_Vertex_Disjoint.cpp
@@ -1,11 +1,18 @@
+/*
+Edge Disjoint BCC
+We are dividing BCC into set of vertices.
+Each BCC doesn't have any articulation edge(bridge).
+Use init(n) at the very first, if needed
+test: https://www.acmicpc.net/problem/11400
+*/
 namespace VertexDisjointBCC{
-    int dfn[N], low[N], pv;
     vector<int> g[N];
     void add_edge(int a,int b){
         g[a].push_back(b);
         g[b].push_back(a);
     }
-    void dfs(int x,int p){
+    int dfn[N], low[N], pv;
+    void dfs(int x,int p=0){
         dfn[x] = low[x] = ++pv;
         for(auto nx: g[x]) if(nx != p){
             if(dfn[nx]) low[x] = min(low[x], dfn[nx]);
@@ -15,7 +22,7 @@ namespace VertexDisjointBCC{
             }
         }
     }
-    int bcnt = 1, bcc[N];
+    int bcnt, bcc[N];
     vector<pii> bridges;
     void color(int x,int col){
         bcc[x] = col;
@@ -27,8 +34,16 @@ namespace VertexDisjointBCC{
             }
         }
     }
+    void init(int n){
+        pv = bcnt = 0;
+        for(int i=1;i<=n;i++){
+            g[i].clear();
+            dfn[i] = low[i] = bcc[i] = 0;
+        }
+        bridges.clear();
+    }
     void get_bcc(int n){
         for(int i=1;i<=n;i++) if(!dfn[i]) dfs(i, 0);
-        for(int i=1;i<=n;i++) if(!bcc[i]) color(i, 1);
+        for(int i=1;i<=n;i++) if(!bcc[i]) color(i, ++bcnt);
     }
 } using namespace VertexDisjointBCC;
