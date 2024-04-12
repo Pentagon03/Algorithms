4cbf9d9276303a07d3ab1d8d1375e19d69c4067c
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-04-12 20:21:50 +0900

    Update SCC_Kosaraju.cpp

--- Graphs/SCC_Kosaraju.cpp
+++ Graphs/SCC_Kosaraju.cpp
@@ -2,6 +2,8 @@
 Find Stronly Connected Components
 Kosaraju's algorithm
 O(V+E)
+use init(n) at the very first, if needed.
+test: https://www.acmicpc.net/problem/2150
 */
 namespace SCC{
     vector<int> g[N], gR[N], S;
@@ -15,10 +17,17 @@ namespace SCC{
         vis[v] = true;
         auto&G = !rev ? g: gR;
         if(rev) scc[v] = scnt;
-        for(auto nxt:G[v]) if(!vis[nxt]) dfs(nxt, rev);
+        for(auto nxt:G[v])
+            if(!vis[nxt])
+                dfs(nxt, rev);
         if(!rev) S.push_back(v);
     }
     vector<vector<int>> SCCs;
+    void init(int n){
+        for(int i=1;i<=n;i++) g[i].clear(), gR[i].clear(), scc[i] = 0;
+        S.clear(); vis.clear();
+        scnt = 0;
+    }
     void get_SCC(int n){
         vis.assign(n+1, false);
         for(int i=1;i<=n;i++) if(!vis[i]) dfs(i, false);
