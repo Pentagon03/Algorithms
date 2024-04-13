0fa8cdf6d417c574c9a4991206848941b3802d84
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-04-13 18:39:41 +0900

    Create BIT.cpp

---
+++ Data Structures/BIT.cpp
@@ -0,0 +1,16 @@
+template<typename T = int>
+struct BIT{
+    int n; vector<T> t;
+    BIT(int _n=0):n(_n),t(_n+1){}
+    void op(T &l,T r){l += r;}
+    void upd(int p,T v){
+        for(;p<=n;p+=p&-p)
+            op(t[p], v);
+    }
+    T qry(int p){
+        T res = 0;
+        for(;p>0;p-=p&-p)
+            op(res, T[p]);
+        return res;
+    }
+};
