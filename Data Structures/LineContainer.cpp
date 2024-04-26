92a157d61e8dc07628aba182739d0bb2227102e9
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-04-26 20:03:57 +0900

    Update LineContainer.cpp

--- Data Structures/LineContainer.cpp
+++ Data Structures/LineContainer.cpp
@@ -1,14 +1,15 @@
 /*
-Line container for dynamic CHT
+Line container for dynamic CHT 
 y = kx + m
-qry: gives minimum value for given x
+qry: gives maximum value for given x, upper hull
 reference: https://github.com/kth-competitive-programming/kactl/blob/main/content/data-structures/LineContainer.h
 find 2 of (//chg here) to change minimum / maximum properties.
 */
 struct Line{
     mutable ll k, m, p;
     // we know that there will be only 'integer' queries
-    bool operator<(const Line& o) const{return k > o.k;} // chg here
+    // Upper Hull
+    bool operator<(const Line& o) const{return k < o.k;} // chg here
     // only for integer coordinates
     bool operator<(ll x) const {return p < x;}
 };
@@ -23,12 +24,12 @@ struct LineContainer : multiset<Line, less<>> {
             x->p = inf;
             return false;
         }
-        if(x->k == y->k) x->p = (x->m < y->m?inf:-inf); // chg here
+        if(x->k == y->k) x->p = (x->m > y->m?inf:-inf); // chg here, bigger m
         else x->p = div(y->m - x->m, x->k - y->k);
         return x->p >= y->p;
     }
     void add(ll k, ll m){
-        auto z = insert({k, m, 0}), y = z++, x = y;
+        auto z = insert({k, m, -inf}), y = z++, x = y;
         while(apply(y, z)) z = erase(z);
         if(x != begin() && apply(--x, y)) apply(x, y = erase(y));
         while((y=x) != begin() && (--x)->p >= y->p)
