a8b0738703549f69ee019fb9f41f0e2e908a80fa
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-04-27 18:34:18 +0900

    Update BIT.cpp

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -1,16 +1,14 @@
 template<typename T = int>
 struct BIT{
     int n; vector<T> t;
-    BIT(int _n=0):n(_n),t(_n+1){}
+    BIT(int _n=0):n(_n),t(_n+2){}
     void op(T &l,T r){l += r;}
     void upd(int p,T v){
-        for(;p<=n;p+=p&-p)
-            op(t[p], v);
+        for(p+=1;p<=n;p+=p&-p) op(t[p], v);
     }
     T qry(int p){
         T res = 0;
-        for(;p>0;p-=p&-p)
-            op(res, t[p]);
+        for(p+=1;p>0;p-=p&-p) op(res, t[p]);
         return res;
     }
 };
