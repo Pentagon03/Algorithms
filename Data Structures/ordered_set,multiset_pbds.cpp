013c27b418f7d7f745de240570f6c6361f509a3c
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-05-03 16:45:35 +0900

    Update ordered_set,multiset_pbds.cpp

--- Data Structures/ordered_set,multiset_pbds.cpp
+++ Data Structures/ordered_set,multiset_pbds.cpp
@@ -2,27 +2,24 @@
 ordered set and ordered multiset inheritted by gnu policy-based-structures
 that you can calculate index of variable, and k-th element in O(log n)
 reference: https://codeforces.com/blog/entry/11080
-
-for making into templates, you should use
-typename parent_ordered_set<T>::iterator
-and change a lot functions to (f => parent_ordered_set<T>::f)
-so I just rather chose to use "using Typename = int;"
+code help: frozenca (https://github.com/frozenca)
 */
+
 // ORDERED_SET START
 #include "ext/pb_ds/assoc_container.hpp"
 using namespace __gnu_pbds;
-
-using Typename = int;
-struct ordered_set : tree<Typename, null_type, less<Typename>, rb_tree_tag,tree_order_statistics_node_update>{
+template <typename Typename = int, class Parent = tree<Typename, null_type, less<Typename>, rb_tree_tag,tree_order_statistics_node_update>>
+struct ordered_set : Parent{
+    using iterator = typename Parent::iterator;
     // this returns the element of idx-th index in O(log n)
     iterator kth_element(int idx){
-        assert(0 <= idx && idx < size());
-        return find_by_order(idx);
+        assert(0 <= idx && idx < Parent::size());
+        return Parent::find_by_order(idx);
     }
     // this returns the index of lower bound of val in O(log n)
     int index(Typename val){
-        assert(size() > 0);
-        return order_of_key(val);
+        assert(Parent::size() > 0);
+        return Parent::order_of_key(val);
     }
 };
 // ORDERED_SET END
@@ -30,34 +27,35 @@ struct ordered_set : tree<Typename, null_type, less<Typename>, rb_tree_tag,tree_
 // ORDERED_MULTISET START
 #include "ext/pb_ds/assoc_container.hpp"
 using namespace __gnu_pbds;
-using Typename = int;
-using parent_ordered_multiset = tree<Typename, null_type, less_equal<Typename>, rb_tree_tag,tree_order_statistics_node_update>;
-struct ordered_multiset : parent_ordered_multiset{
+
+template <typename Typename = int, class Parent = tree<Typename, null_type, less_equal<Typename>, rb_tree_tag,tree_order_statistics_node_update>>
+struct ordered_multiset : Parent{
     /*
         for multiset, functions of lower_bound and upper_bound swap.
         see the declaration
         lower_bound: smallest idx that "a[idx] '<=' val" is false; thus val < a[idx]
         upper_bound: smallest idx that "val '<=' a[i]" is true; thus val <= a[idx]
     */
-    iterator lower_bound(Typename val){return parent_ordered_multiset::upper_bound(val);}
-    iterator upper_bound(Typename val){return parent_ordered_multiset::lower_bound(val);}
-    void erase(iterator it){parent_ordered_multiset::erase(it);}
+    using iterator = typename Parent::iterator;
+    iterator lower_bound(Typename val){return Parent::upper_bound(val);}
+    iterator upper_bound(Typename val){return Parent::lower_bound(val);}
+    void erase(iterator it){Parent::erase(it);}
     bool erase(Typename val){
-        auto it = lower_bound(val);
-        if(it != end() && *it == val){
-            erase(it); return true;
+        auto it = Parent::lower_bound(val);
+        if(it != Parent::end() && *it == val){
+            Parent::erase(it); return true;
         }
         return false;
     }
     // this returns the element of idx-th index in O(log n)
     iterator kth_element(int idx){
-        assert(0 <= idx && idx < size());
-        return find_by_order(idx);
+        assert(0 <= idx && idx < Parent::size());
+        return Parent::find_by_order(idx);
     }
     // this returns the index of lower bound of val in O(log n)
     int index(Typename val){
-        assert(size() > 0);
-        return order_of_key(val);
+        assert(Parent::size() > 0);
+        return Parent::order_of_key(val);
     }
 };
 // ORDERED_MULTISET END
