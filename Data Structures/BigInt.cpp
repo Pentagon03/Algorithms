5188f52475ef6a0ff4b8182268990c6d0703388f
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-05-04 22:47:47 +0900

    Update BigInt.cpp

--- Data Structures/BigInt.cpp
+++ Data Structures/BigInt.cpp
@@ -1,7 +1,9 @@
 // [Option 1]
+// test: http://boj.kr/12401ac7a9414dc8b96b8c2ebbf6eb49
+// watch here: https://github.com/ngthanhtrung23/ACM_Notebook_new/blob/master/Math/bigint.h
+
+// [Option 2]
 // test: http://boj.kr/43fa901846c44b828b13e8b4fa452579
 // watch here: https://github.com/sercantutar/infint/blob/master/InfInt.h
 
-// [Option 2]
-// test: http://boj.kr/12401ac7a9414dc8b96b8c2ebbf6eb49
-// watch here: https://github.com/ngthanhtrung23/ACM_Notebook_new/blob/master/Math/bigint.h
+
