556ccfdb56c4a95901f353e4e4d4129d24ff51c4
Pentagon03 <kyungmoku7141@gmail.com>
2024-05-10 01:41:27 +0900

    minor

---
+++ Etc/Template.cpp
@@ -0,0 +1,47 @@
+#define LLHACK
+/*Solution
+https://acmicpc.net/problem/Template
+1. 문제부터 정확히 읽는다.
+2. 안 풀리면 naive 풀이 짜고 관찰한다.
+3. 구현 틀렸으면 stress test 짠다.
+화이팅
+*/
+#pragma GCC optimize("Ofast,unroll-loops")
+#include"bits/stdc++.h"
+using namespace std;
+#define nl '\n'
+#define sp ' '
+#define dbg(i) cerr<<#i<<sp<<i<<endl
+#define dbgs(...) fprintf(stderr, __VA_ARGS__)
+#define all(v) (v).begin(),(v).end()
+using ld = long double;
+using ll = long long;
+#ifdef LLHACK
+#define int ll
+#endif
+using pii = pair<int,int>;
+int cur_tc;
+std::mt19937_64 gen(time(NULL));
+ll randint(ll l, ll r) { std::uniform_int_distribution<ll> dist(l, r); return dist(gen);}
+template<class T1,class T2>bool updmax(T1&l,T2 r){return (l<r?(l=r),true: false);}
+template<class T1,class T2>bool updmin(T1&l,T2 r){return (l>r?(l=r),true: false);}
+const int inf = (int)1e9 + 7;
+const ll linf = (ll)4e18 + 7;
+
+const int N = 7 + 1e5, mod = (int)1e9 + 7;
+
+/*
+요약: 
+*/
+void _main(){
+    
+}
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+    // cout<<fixed<<setprecision(15);
+    int tc = 1;
+    // cin >> tc;
+    for(cur_tc=1;cur_tc<=tc;++cur_tc) _main();
+    return 0;
+}
