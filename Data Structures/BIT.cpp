9332ba7987fd711ebcf5b57122fde15f43160116
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-05-20 01:39:18 +0900

    Update BIT.cpp

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -5,7 +5,7 @@ set your operation function op
 template<typename T = int>
 struct BIT{
     int n; vector<T> t;
-    BIT(int _n=0):n(_n),t(_n+2){}
+    BIT(int _n=0):n(_n),t(_n+4){}
     void op(T &l,T r){l += r;}
     void upd(int p,T v){
         for(p+=1;p<=n;p+=p&-p) op(t[p], v);
