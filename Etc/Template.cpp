6b7fcd604d709417568aef09ab4f08297ee89ba1
Pentagon03 <kyungmoku7141@gmail.com>
2024-06-14 11:39:28 +0900

    snippet

--- Etc/Template.cpp
+++ Etc/Template.cpp
@@ -1,53 +1,25 @@
-#define LLHACK
-/*Solution
-Problem: Template
-1. 문제부터 정확히 읽는다.
-2. 수단과 방법을 가리지 말고 문제를 먼저 맞춘다
-3. 잘 안되면 세수라도 하고 오거나 stress test를 짠다.
-화이팅
-*/
 #pragma GCC optimize("Ofast,unroll-loops")
 #include"bits/stdc++.h"
 using namespace std;
-int cur_tc;
 #define nl '\n'
 #define sp ' '
-#define dbg(i) cerr<<#i<<sp<<i<<endl
-#define dbgs(...) fprintf(stderr, __VA_ARGS__)
 #define all(v) (v).begin(),(v).end()
-using ld = long double;
-using ll = long long;
-#ifdef LLHACK
+#define INIT(v, x) memset(v, x, sizeof v)
+int _dbg, cur_tc;
+#define dbg(i) if(!_dbg||_dbg==cur_tc) cerr << "Line(" << __LINE__ << ") -> " << #i << " = " << (i) << endl;
+#define dbgs(...) if(!_dbg||_dbg==cur_tc)fprintf(stderr, __VA_ARGS__)
+template<typename T> istream& operator>> (istream&in, vector<T>&v){for(auto&x:v)in>>x; return in;}
+using ll = int64_t;
 #define int ll
-#endif
-using pii = pair<int,int>;
-// __builtin_ctzll(x), __builtin_clzll(x), __builtin_popcountll(x)
-int lsb(int x){return x>0?__builtin_ctz(x):-1;}
-int msb(int x){return x>0?31-__builtin_clz(x):-1;}
-int popcnt(int x){return __builtin_popcount(x);}
-std::mt19937_64 gen(time(NULL));
-ll randint(ll l, ll r) { std::uniform_int_distribution<ll> dist(l, r); return dist(gen);}
-template<class T1,class T2>bool updmax(T1&l,T2 r){return (l<r?(l=r),true: false);}
-template<class T1,class T2>bool updmin(T1&l,T2 r){return (l>r?(l=r),true: false);}
-const int inf = (int)1e9 + 7;
-const ll linf = (ll)4e18 + 7;
 
-/*요약: 문제의 본질을 본다
-
-*/
-const int N = 7 + 1e5, mod = (int)1e9 + 7;
-
-void _main(){
-    
-}
-
-signed main(){
-    cin.tie(0)->sync_with_stdio(0);
-    cin.exceptions(ios::badbit | ios::failbit);
+int32_t main(){
+    cin.tie(0)->sync_with_stdio(0); cin.exceptions(ios::badbit | ios::failbit);
     // cout<<fixed<<setprecision(15);
     int tc = 1;
     // cin >> tc;
-    for(cur_tc=1;cur_tc<=tc;++cur_tc) _main();
+    auto solve_tc = [&](){
+        
+    };
+    for(cur_tc=1;cur_tc<=tc;cur_tc++) solve_tc();
     return 0;
 }
-// https://github.com/Pentagon03/Algorithms
