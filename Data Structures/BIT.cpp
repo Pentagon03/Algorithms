6346eb703eb751a42af1e5b40df5cd577e659399
Pentagon03 <kyungmoku7141@gmail.com>
2024-07-19 11:19:23 +0900

    snippets

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -6,15 +6,15 @@ template<typename T = int>
 struct BIT{
     int n; vector<T> t;
     BIT(int _n=0):n(_n+4),t(n+1){}
-    void op(T &l,T r){l += r;}
-    void upd(int p,T v){
-        assert(p+2 <= n);
-        for(p+=2;p<=n;p+=p&-p) op(t[p], v);
+    inline void op(T &l,T r){l += r;}
+    inline void upd(int p,T v){
+        assert(1 <= p+2 && p+2 <= n);
+        for(p+=2;p<=n;p+=p&-p) op(t[p],v);
     }
-    T qry(int p){
-        assert(p+2 <= n);
-        T res = 0;
-        for(p+=2;p>0;p-=p&-p) op(res, t[p]);
+    inline T qry(int p){
+        assert(1 <= p+2 && p+2 <= n);
+        T res=0;
+        for(p+=2;p>0;p-=p&-p) op(res,t[p]);
         return res;
     }
 };
