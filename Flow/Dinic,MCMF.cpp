9b7ea6fdceb8aa3efe3747c117793d1008a38aeb
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-08-31 11:46:37 +0900

    Update and rename MCMF.cpp to Dinic,MCMF.cpp

--- Flow/Dinic,MCMF.cpp
+++ Flow/Dinic,MCMF.cpp
@@ -1,3 +1,72 @@
+// hijkl2e, https://www.acmicpc.net/source/59744372
+
+using ll = long long;
+
+const ll INF = 4e18;
+
+class MF {
+public:
+	MF(int V) : G(V), cap(V, vector<ll>(V)), flow(V, vector<ll>(V)), d(V), last(V) { }
+	void add_edge(int u, int v, ll c, bool d = true) {
+		G[u].push_back(v);
+		G[v].push_back(u);
+		cap[u][v] = c;
+		cap[v][u] = d ? 0 : c;
+	}
+	ll dinic(int s, int t) {
+		ll mf{};
+		while (bfs(s, t)) {
+			fill(last.begin(), last.end(), 0);
+			while (ll f = dfs(s, t)) {
+				mf += f;
+			}
+		}
+		return mf;
+	}
+	vector<int> d;
+private:
+	bool bfs(int s, int t) {
+		fill(d.begin(), d.end(), -1);
+		queue<int> q;
+		d[s] = 0;
+		q.push(s);
+		while (q.size()) {
+			int u = q.front(); q.pop();
+			for (int v : G[u]) {
+				if (d[v] == -1 && flow[u][v] < cap[u][v]) {
+					d[v] = d[u] + 1;
+					q.push(v);
+					if (v == t) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	ll dfs(int u, int t, ll f = INF) {
+		if (u == t) {
+			return f;
+		}
+		for (int &i = last[u]; i < G[u].size(); ++i) {
+			int v = G[u][i];
+			if (d[v] == d[u] + 1 && flow[u][v] < cap[u][v]) {
+				if (ll pushed = dfs(v, t, min(f, cap[u][v] - flow[u][v]))) {
+					flow[u][v] += pushed;
+					flow[v][u] -= pushed;
+					return pushed;
+				}
+			}
+		}
+		return 0;
+	}
+	vector<vector<int>> G;
+	vector<vector<ll>> cap, flow;
+	vector<int> last;
+};
+
+
+
 // hijkl2e, https://www.acmicpc.net/source/62155473
 template<typename Cap, typename Cost>
 class mcf_graph {
