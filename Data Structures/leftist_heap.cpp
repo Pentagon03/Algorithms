8aa002b0211d51a19e18c20544713aa9d723d58f
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-09-22 23:56:40 +0900

    Update leftist_heap.cpp

--- Data Structures/leftist_heap.cpp
+++ Data Structures/leftist_heap.cpp
@@ -3,7 +3,7 @@
 #include<algorithm>
 
 /**
- * Title: "Min" Leftist Heap which is Persistent.
+ * Title: "Min" Persistent Leftist Heap
  * Repository: https://github.com/Pentagon03/Algorithms/blob/master/Data%20Structures/leftist_heap.cpp
  * Reference: nor( https://judge.yosupo.jp/submission/87297 )
  */
