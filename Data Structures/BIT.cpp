65c82beef11ca0f84813059f76a84a7e7a4fac16
Pentagon03 <kyungmoku7141@gmail.com>
2024-09-24 13:15:08 +0900

    BIT bug

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -6,7 +6,7 @@ template<typename T = int>
 struct BIT{
     int n; vector<T> t;
     BIT(int _n=0):n(_n+4),t(n+1){}
-    inline void op(T &l,T r){l += r;}
+    inline void op(T &l,T r) const{l += r;}
     inline void upd(int p,T v){
         assert(1 <= p+2 && p+2 <= n);
         for(p+=2;p<=n;p+=p&-p) op(t[p],v);
