fadc9bbd323eaf9e5da33511b707cfc867257f1b
Pentagon03 <kyungmoku7141@gmail.com>
2024-10-30 12:17:33 +0900

    modify

--- Data Structures/SOS_DP.cpp
+++ Data Structures/SOS_DP.cpp
@@ -1,4 +1,5 @@
-// Sum of Subset DP
+// SOS DP
+
 // F[mask] = sum of A[i] where i is subset of mask
 // 1. Naive, O(3^N)
 F[mask] = A[0];
@@ -11,9 +12,10 @@ for(int i=0;i<N;i++) for(int mask=0;mask<(1<<N);mask++)
 	if(mask & (1<<i))
 		F[mask] += F[mask ^ (1<<i)]  
 
-// 3. Next Lexicographical bit with same bit count
-int mxb = 30; // max bit count
-int x; // popcount(x) 개의 비트를 가지는 수들을 x부터 사전순으로 순회
-for(int mx = (1<<mxb)-1;x <= mx;c = x&-x, r = x+c, x =  ( ((rˆx) >> 2)/c) | r) ){
+// 3. Next Lexicographical number with same bit count
+int mxb = 30, mx = (1<<mxb) - 1; // max bit count
+int b; // b개의 비트를 가지는 수들을 x부터 사전순으로 순회
+// 혹은 x = mask로 시작
+for(int x = (1<<b)-1; x <= mx; c = x&-x, r = x+c, x = ( (((rˆx) >> 2)/c) | r) ){
   // do smth with x
 }
