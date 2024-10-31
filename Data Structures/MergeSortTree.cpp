e5f0cb00837a68eabcb83e38c1462203fa967e2f
Pentagon03 <kyungmoku7141@gmail.com>
2024-10-31 20:55:09 +0900

    modify

--- Data Structures/MergeSortTree.cpp
+++ Data Structures/MergeSortTree.cpp
@@ -1,37 +1,67 @@
-/*
-Basic Merge Sort Tree
-*/
+template<typename T = int>
 struct MergeSortTree{
-    int n; 
-    vector<vector<int>> tree;
-    MergeSortTree(int k = 0){
-        n = k;
-        int st = 1; while(st < n) st <<= 1;
-        tree.resize(st<<1);
+    using Vec = vector<T>;
+    int n, base;
+    vector<Vec> tree;
+    MergeSortTree(){}
+    MergeSortTree(const Vec& v){
+        n = v.size();
+        base = 1 << __lg(n-1) + 1;
+        tree.resize(base<<1);
+        for(int i=0;i<n;i++)
+            tree[base+i] = {v[i]};
+        for(int i=base-1;i>=1;i--)
+            merge(all(tree[i<<1]),all(tree[i<<1|1]),back_inserter(tree[i]));
     }
-    MergeSortTree(const vector<int>&v){
-        *this = MergeSortTree(v.size());
-        init(v, 1, 0, n-1);
+    int elem(int k){
+        assert(0 <= k and k < n);
+        return tree[base+k][0];
     }
-    void init(const vector<int>&v,int nd,int s,int e){
-        if(e==s){
-            tree[nd].push_back(v[s]);
-            return;
+    inline int greater(const Vec&v, int x){
+        return v.end() - upper_bound(all(v), x);
+    }
+    inline int less(const Vec&v, int x){
+        return lower_bound(all(v), x) - v.begin();
+    }
+    inline int greater_equal(const Vec&v, int x){
+        return v.size() - less(v, x); 
+    }
+    inline int less_equal(const Vec&v,  int x){
+        return v.size() - greater(v, x);
+    }
+    inline int in_range(const Vec&v,  int x, int y){
+        if(x > y) return 0;
+        return upper_bound(all(v), y) - lower_bound(all(v), x);
+    }
+    inline int equal(const Vec&v, int x){
+        return in_range(v, x, x);
+    }
+    /* op is an operation to a vector
+    ex) cnt_qry(0, n-1, [&](const vec& v){return f.less(v, x);})
+    */
+    // O(log n * T(op))
+    int cnt_qry(int l,int r,const function<int(const Vec&)>& op){
+        int ans = 0;
+        l+=base; r+=base;
+        while(l<=r){
+            if(l&1) ans += op(tree[l++]);
+            if(~r&1) ans += op(tree[r--]);
+            l>>=1; r>>=1;
         }
-        int m = s + e >> 1;
-        init(v,nd<<1,s,m); init(v,nd<<1|1,m+1,e);
-        merge(all(tree[nd<<1]),all(tree[nd<<1|1]),back_inserter(tree[nd]));
+        return ans;
+    }   
+    // max t that op([l, t]) <= k.  O(log^2 n * T(op))
+    int rightmost(int l, int k, const function<int(const Vec&)>& op){
+        if(k < 0) return l - 1;
+        int lo = l, hi = n, mid;
+        while(lo+1<hi) cnt_qry(l, mid = lo + hi >> 1, op) <= k ? lo = mid : hi = mid; 
+        return lo;
     }
-    // [a,b] in nd
-    inline int count(int nd, int a, int b){
-        return upper_bound(all(tree[nd]),b) - lower_bound(all(tree[nd]),a);
-    }
-    int query(int k,int a,int b){return query(k,a,b,1,0,n-1);}
-    int query(int k,int a,int b,int nd,int s,int e){
-        if(e==s) return s;
-        int cnt = count(nd<<1, a, b);
-        int m = s + e >> 1;
-        if(cnt >= k) return query(k,a,b,nd<<1,s,m);
-        else return query(k-cnt,a,b,nd<<1|1,m+1,e);
+    // min t that op([t, r], x) <= k. O(log^2 n * T(op))
+    int leftmost(int r, int k, const function<int(const Vec&)>& op){
+        if(k < 0) return r+1;
+        int lo = -1, hi = r, mid;
+        while(lo+1<hi) cnt_qry(mid = lo + hi >> 1, r, op) <= k ? hi = mid : lo = mid;
+        return hi;
     }
 };
