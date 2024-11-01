54ebbaa3f43d607cbe0fc69bdf390cc2deda4331
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-01 17:51:34 +0900

    modify

--- Etc/short_debug.cpp
+++ Etc/short_debug.cpp
@@ -1,4 +1,8 @@
 // https://codeforces.com/blog/entry/91347?#comment-798799
+#ifndef ONLINE_JUDGE
 #define gg(...) [](const auto&...x){ \
 char c='='; cerr<<#__VA_ARGS__<<" "; ((cerr<<exchange(c,',')<<" "<<x),...); cerr<<endl;\
 }(__VA_ARGS__);
+#else
+#define gg(...)
+#endif
