4616613fd2619145bf2163eb9c88de3b66a1a563
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-03 17:26:00 +0900

    modify

--- Etc/short_debug.cpp
+++ Etc/short_debug.cpp
@@ -1,4 +1,4 @@
-// https://codeforces.com/blog/entry/91347?#comment-798799
+// source: https://codeforces.com/blog/entry/91347?#comment-798799
 #ifndef ONLINE_JUDGE
 #define gg(...) [](const auto&...x){ \
 char c='='; cerr<<#__VA_ARGS__<<" "; ((cerr<<exchange(c,',')<<" "<<x),...); cerr<<endl;\
@@ -6,3 +6,12 @@ char c='='; cerr<<#__VA_ARGS__<<" "; ((cerr<<exchange(c,',')<<" "<<x),...); cerr
 #else
 #define gg(...)
 #endif
+
+// source: swoon
+template<class T>
+void prt(T &&v) { cout << forward<T>(v) << '\n'; }
+template<class T, class... Args>
+void prt(T &&v, Args &&...args) {
+    cout << forward<T>(v) << ' ';
+    prt(forward<Args>(args)...);
+};
