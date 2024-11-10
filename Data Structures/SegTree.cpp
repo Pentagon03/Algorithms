99dfd63da7bb9bbe68a0e6fc15c4970301aae36a
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-10 19:25:17 +0900

    modify

--- Data Structures/SegTree.cpp
+++ Data Structures/SegTree.cpp
@@ -3,19 +3,19 @@ Segment Tree Generalized
 Usage: SegTree<int> st(n, less<int>(), INT_MAX); or lambda
 Reference: https://codeforces.com/contest/1936/submission/249337780
 */
-template<typename T, class Func = function<T(const T &, const T &)>>
+template<class T, class Func = function<T(const T &, const T &)>>
 struct SegTree{
     uint32_t n; vector<T> t;
     T identity;
     Func f;
-    SegTree(uint32_t _n, const Func &g, T dval = T()):f(g), identity(dval){
+    SegTree(uint32_t _n, const Func &g = Func(), T dval = T()):f(g), identity(dval){
         assert(_n >= 1);
         // n = 1;
         // while(n < _n) n <<= 1;
         n = bit_ceil(_n);
         t.assign(n*2, identity);
     }
-    SegTree(const vector<T>&v, const Func &g, T dval = T()): SegTree(size(v), g, dval){
+    SegTree(const vector<T>&v, const Func &g = Func(), T dval = T()): SegTree(size(v), g, dval){
         for(int i=0;i<size(v);i++) t[i+n]=v[i];
         for(int i=n-1;i>=1;i--) t[i]=f(t[i<<1],t[i<<1|1]);
     }
