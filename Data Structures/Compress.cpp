86883b61068526ad8e60d7dc6c656b3f7f2b3570
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-11 02:11:38 +0900

    modify

---
+++ Data Structures/Compress.cpp
@@ -0,0 +1,20 @@
+// reference: https://sotanishy.github.io/cp-library-cpp/misc/compress.hpp.html
+template<class T>
+struct Compress{
+    vector<T> t;
+    Compress() = default;
+    Compress(const vector<T> &v): t(v){}
+    void add(const auto&... x){ ((t.push_back(x)), ...); }
+    void compress(){
+        ranges::sort(t);
+        t.erase(begin(ranges::unique(t)), end(t));
+    }
+    int compress(const T& x) const {
+        return ranges::distance(begin(t), ranges::lower_bound(t, x));
+    }
+    void compress(vector<T> &v) const {
+        ranges::transform(v, begin(v), [&](T x){return compress(x);});
+    }
+    const T& val(int i) const { return t[i]; }
+    int size() const{ return t.size(); }
+};
---
+++ Data Structures/SweepLine_SegTree.cpp
@@ -0,0 +1,45 @@
+// reference: https://www.acmicpc.net/source/31632142
+template<class T>
+struct SweepLine{
+    const vector<T>& c;
+    unsigned n;
+    struct Node{
+        int cover;
+        T sum;
+    };
+    vector<Node> tree;
+    SweepLine(const vector<T>& compressed): c(compressed), n(size(c)-1), tree(bit_ceil(n)<<1){}
+    // [l, r), [s, e)
+    void upd(int l, int r, int x, int nd, int s, int e){
+        if(e <= l or r <= s) return;
+        if(l <= s and e <= r){
+            tree[nd].cover += x;
+        }else{
+            int mid = midpoint(s, e);
+            upd(l, r, x, nd << 1, s, mid);
+            upd(l, r, x, nd << 1 | 1, mid, e);
+        }
+        if(tree[nd].cover > 0) tree[nd].sum = c[e] - c[s];
+        else tree[nd].sum = e - s > 1 ? tree[nd<<1].sum + tree[nd<<1|1].sum : 0;
+    }
+    // [l, r)
+    void upd(int l, int r, int x){
+        assert(0 <= l and r <= n);
+        upd(l, r, x, 1, 0, n);
+    }
+    // [l, r), [s, e)
+    int qry(int l, int r, int nd, int s, int e){
+        if(e <= l or r <= s) return 0;
+        if(l <= s and e <= r) return tree[nd].sum;
+        if(tree[nd].cover > 0){
+            int lo = max(l, s), hi = min(r, e);
+            return c[hi] - c[lo];
+        }
+        int mid = midpoint(s, e);
+        return qry(l, r, nd << 1, s, mid) + qry(l, r, nd << 1 | 1, mid, e);
+    }
+    int qry(int l, int r){
+        assert(0 <= l and r <= n);
+        return qry(l, r, 1, 0, n);
+    }
+};
