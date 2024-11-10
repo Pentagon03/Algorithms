376a0d490429c8ae6db04f07bd493ead82462044
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-10 12:24:41 +0900

    fast push.bat

--- make_snippet.bat
+++ make_snippet.bat
@@ -2,8 +2,10 @@
 REM Python 스크립트를 실행하여 스니펫 파일 생성
 python Etc/_vscode_snippet_generator.py
 
+REM 기존에 있던 스니펫을 old 폴더로 이동
 IF EXIST "C:\Users\KyungmoKu\AppData\Roaming\Code\User\snippets\algo.code-snippets" (
     move /Y "C:\Users\KyungmoKu\AppData\Roaming\Code\User\snippets\algo.code-snippets" "C:\Users\KyungmoKu\AppData\Roaming\Code\User\snippets\old"
 )
 
+REM 새로 만든 스니펫으로 교체
 copy /Y "algo.code-snippets" "C:\Users\KyungmoKu\AppData\Roaming\Code\User\snippets"
