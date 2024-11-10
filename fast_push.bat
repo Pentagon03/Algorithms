3cf97de363886e0763f72da806096f194ec9f347
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-10 12:25:20 +0900

    fast_push.bat

--- fast_push.bat
+++ fast_push.bat
@@ -1,13 +1,5 @@
-@echo off
-chcp 65001 >nul
-setlocal
 call make_snippet.bat
 git pull
 git add .
-
-rem 사용자에게 간단한 수정 내용을 입력받기
-set /p commit_message="Enter commit message (description of changes): "
-
 git commit -m "%commit_message%"
 git push
-endlocal
