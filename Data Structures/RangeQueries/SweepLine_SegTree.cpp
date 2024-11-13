e7c4e8dac410dca68a29704be50c5701ed77de50
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-13 19:47:46 +0900

    modify

--- Data Structures/RangeQueries/SweepLine_SegTree.cpp
+++ Data Structures/RangeQueries/SweepLine_SegTree.cpp
@@ -10,7 +10,8 @@ struct SweepLine{
     vector<Node> tree;
     SweepLine(const vector<T>& compressed): c(compressed), n(size(c)-1), tree(bit_ceil(n)<<1){}
     // [l, r), [s, e)
-    void upd(int l, int r, int x, int nd, int s, int e){
+    void upd(int l, int r, int x, int nd = 1, int s = 0, int e = -1){
+        if(e == -1) e = n;
         if(e <= l or r <= s) return;
         if(l <= s and e <= r){
             tree[nd].cover += x;
@@ -22,13 +23,9 @@ struct SweepLine{
         if(tree[nd].cover > 0) tree[nd].sum = c[e] - c[s];
         else tree[nd].sum = e - s > 1 ? tree[nd<<1].sum + tree[nd<<1|1].sum : 0;
     }
-    // [l, r)
-    void upd(int l, int r, int x){
-        assert(0 <= l and r <= n);
-        upd(l, r, x, 1, 0, n);
-    }
     // [l, r), [s, e)
-    int qry(int l, int r, int nd, int s, int e){
+    int qry(int l, int r, int nd = 1, int s = 0, int e = -1){
+        if(e == -1) e = n;
         if(e <= l or r <= s) return 0;
         if(l <= s and e <= r) return tree[nd].sum;
         if(tree[nd].cover > 0){
@@ -37,9 +34,5 @@ struct SweepLine{
         }
         int mid = midpoint(s, e);
         return qry(l, r, nd << 1, s, mid) + qry(l, r, nd << 1 | 1, mid, e);
-    }
-    int qry(int l, int r){
-        assert(0 <= l and r <= n);
-        return qry(l, r, 1, 0, n);
     }
 };
---
+++ Data Structures/RangeQueries/lazyseg_examples.md
@@ -0,0 +1,9 @@
+# Lazy Segtree
+- put 'fail' information for Segment Tree Beats
+- S: monoid for tree
+- S op(S a, S b): monoid action
+- S e() : identity of S
+- F: mapping function
+- S mapping(F f, S x) : f(x)
+- F composition(F f, F g): f(g(x)) -> h(x)
+- F id(): identity of f
