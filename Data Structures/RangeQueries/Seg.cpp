665b49867a2c65e40adfa6c4b9fcd5d14f80024c
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-17 22:51:23 +0900

    modify

--- Data Structures/RangeQueries/Seg.cpp
+++ Data Structures/RangeQueries/Seg.cpp
@@ -3,6 +3,8 @@ S: monoid for tree
 S e() : identity of S
 S op(S a, S b): monoid action
 Seg<S, e, op>
+`upd(i, val);`
+`qry(l, r);`
 */
 
 $0
@@ -10,7 +12,7 @@ $0
 // S : Range add
 using S = int;
 S e(){return 0;}
-S op(S a, S b){return a + b;}
+S op(const S&a, const S&b){return a + b;}
 
 // template<class S, S (*e)(), S (*op)(S, S)>
 struct Seg{
@@ -18,13 +20,13 @@ struct Seg{
     Seg(int n = 0): Seg(vector (n, e())){}
     Seg(const vector<S>&v){
         // n = bit_ceil(size(v));
-        n = size(v);
+        n = ssize(v);
         tree = vector (n<<1, e());
         ranges::copy(v, begin(tree)+n);
         for(int i=n-1;i>=1;i--) pull(i);
     }
     const S& get(int i) const{return tree[i+n];}
-    void set(int i, S val){
+    void upd(int i, S val){
         tree[i+=n] = val;
         for(i>>=1;i>=1;i>>=1) pull(i);
     }
