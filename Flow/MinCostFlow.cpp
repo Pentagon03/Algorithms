ab98f003c226608fac92649f4d65b53e1d8113c1
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-19 15:36:33 +0900

    modify

--- Flow/MinCostFlow.cpp
+++ Flow/MinCostFlow.cpp
@@ -1,79 +1,122 @@
-// Edited, source: hijkl2e, https://www.acmicpc.net/source/62155473
-// O(VEf) but average O(Ef)
-// Test: https://www.acmicpc.net/source/83574185
-template<typename Cost = int>
+/*
+# Min Cost Flow
+- mcf_graph<cap, cost> fg(V);
+- R: struct{Cap, Cost};
+- R fg.flow(source, sink, flow_limit, cost_limit, paths_limit)
+- vector<R> fg.slope(source, sink, flow_limit, cost_limit, paths_limit)
+- finds flow until flow <= flow_limit.
+- find only paths such that cost per flow <= cost_limit.
+- finds Augmenting path, less or equal to paths_limit times.
+- reference: hijkl2e, https://www.acmicpc.net/source/62155473
+*/
+// using Cap = int32_t; using Cost = int64_t;
+template<typename Cap, typename Cost>
 class mcf_graph {
-public:
-    static constexpr Cost inf = numeric_limits<Cost>::max() / 4;
-	static constexpr int iinf = numeric_limits<int>::max() / 2;
-    using edge = tuple<int, int, int, Cost>;
-    vector<edge> E;
-    vector<vector<int>> G;
-    vector<int> last;
-    vector<bool> vst;
-    vector<Cost> d;
-    Cost tcost{}, mxcost{};
-    mcf_graph(int V) : G(V), d(V), last(V), vst(V){ }
-    void add_edge(int u, int v, int cap, Cost cost, bool d = true) {
-        G[u].push_back(E.size());
-        E.push_back({v, cap, 0, cost});
-        G[v].push_back(E.size());
-        E.push_back({u, 0, 0, -cost});
-        if (!d) add_edge(v, u, cap, cost);
-    }
-    // put _mxcost = inf for no constrains for _mxcost
-    pair<int, Cost> flow(int s, int t, Cost _mxcost = inf) {
-        mxcost = _mxcost;
-        int mf{};
-        while (spfa(s, t)) {
-            fill(last.begin(), last.end(), 0);
-            while (int f = dfs(s, t, iinf)) {
-                mf += f;
-            }
-        }
-        return {mf, tcost};
-    }
 private:
-    bool spfa(int s, int t) {
-        fill(d.begin(), d.end(), inf);
-        queue<int> q;
-        d[s] = 0, vst[s] = true;
-        q.push(s);
-        while (q.size()) {
-            int u = q.front(); q.pop();
-            vst[u] = false;
-            for (int idx : G[u]) {
-                auto &[v, cap, flow, cost] = E[idx];
-                if (d[v] > d[u] + cost && flow < cap) {
-                    d[v] = d[u] + cost;
-                    if (!vst[v]) {
-                        vst[v] = true;
-                        q.push(v);
-                    }
-                }
+    struct _edge{int to; Cap cap; Cost cost;};
+	vector<_edge> E;
+	vector<vector<int>> G;
+	vector<Cost> d; vector<bool> vst;
+    vector<int> last;
+	bool spfa(int s, int t, Cost cost_limit) {
+		fill(d.begin(), d.end(), cost_inf);
+		queue<int> q({s}); d[s] = 0; vst[s] = true;
+		while (q.size()) {
+			int u = q.front(); q.pop();
+			vst[u] = false;
+			for (int idx : G[u]) {
+				auto &[v, cap, cost] = E[idx];
+				if (d[v] > d[u] + cost && cap > 0) {
+					d[v] = d[u] + cost;
+					if (not vst[v]) {
+						vst[v] = true;
+						q.push(v);
+					}
+				}
+			}
+		}
+		return d[t] <= cost_limit;
+	}
+	Cap dfs(int u, int t, Cap f) {
+		if (u == t) return f;
+		vst[u] = true;
+		for (int &i = last[u]; i < G[u].size(); ++i) {
+			auto &[v, cap, cost] = E[G[u][i]];
+			if (not vst[v] and d[v] == d[u] + cost && cap > 0) {
+				if (Cap pushed = dfs(v, t, min(f, cap))) {
+					cap -= pushed;
+					auto &rcap = E[G[u][i] ^ 1].cap;
+					rcap += pushed;
+					vst[u] = false;
+					return pushed;
+				}
+			}
+		}
+		vst[u] = false;
+		return 0;
+	}
+public:
+    static constexpr Cap flow_inf = numeric_limits<Cap>::max();
+    static constexpr Cost cost_inf = numeric_limits<Cost>::max() / 2;
+    static constexpr int paths_inf = numeric_limits<int>::max();
+	mcf_graph(int V) : G(V), d(V), last(V), vst(V){}
+	void add_edge(int u, int v, Cap cap, Cost cost) {
+        assert(0 <= u and u < ssize(G) and 0 <= v and v < ssize(G));
+		G[u].push_back(E.size());
+		E.push_back({v, cap, cost});
+		G[v].push_back(E.size());
+		E.push_back({u, 0, -cost});
+	}
+    struct R{Cap cap; Cost cost;};
+    // returns a Convex down (x = Cap, y = cost) piecewise segments 
+    vector<R> slope(int s, int t, Cap flow_limit = flow_inf, Cost cost_limit = cost_inf - 1, int paths_limit = paths_inf){
+        assert(s != t);
+        Cap flow{}; Cost cost{};
+        vector<R> ans({R{flow, cost}});
+        int path_count = 0;
+        while(flow < flow_limit and path_count < paths_limit){
+            if(bool res = spfa(s, t, cost_limit); not res) break;
+            fill(last.begin(), last.end(), 0);
+            while(flow < flow_limit and path_count < paths_limit){
+                Cap f = dfs(s, t, flow_limit - flow);
+                if(not f) break;
+                flow += f;
+                cost += f * d[t];
+                ans.push_back(R{flow, cost});
+                ++path_count;
             }
         }
-        return d[t] <= mxcost;
+		return ans;
     }
-    int dfs(int u, int t, int f) {
-        if (u == t) {
-            return f;
-        }
-        vst[u] = true;
-        for (int &i = last[u]; i < G[u].size(); ++i) {
-            auto &[v, cap, flow, cost] = E[G[u][i]];
-            if (!vst[v] && d[v] == d[u] + cost && flow < cap) {
-                if (int pushed = dfs(v, t, min(f, cap - flow))) {
-                    tcost += pushed * cost;
-                    flow += pushed;
-                    auto &rflow = get<2>(E[G[u][i] ^ 1]);
-                    rflow -= pushed;
-                    vst[u] = false;
-                    return pushed;
-                }
-            }
-        }
-        vst[u] = false;
-        return 0;
+	R flow(int s, int t, Cap flow_limit = flow_inf, Cost cost_limit = cost_inf - 1, int paths_limit = paths_inf) {
+        assert(s != t);
+        return slope(s, t, flow_limit, cost_limit, paths_limit).back();
+	}
+    struct edge{
+        int from, to; 
+        Cap cap, flow;
+        Cost cost;
+    };
+    edge get_edge(int i){
+        i *= 2;
+        assert(0 <= i and i < E.size());
+        auto[to, cap, cost] = E[i];
+        auto[from, rcap, rcost] = E[i^1];
+        return edge{from, to, cap + rcap, rcap, cost};
+    }
+	vector<edge> edges(){
+		int m = ssize(E) / 2;
+		vector<edge> es(m);
+		for(int i=0;i<m;i++) es[i] = get_edge(i);
+		return move(es);
+	}
+    void change_edge(int i, Cap new_cap, Cap new_flow, Cost new_cost) {
+        i *= 2; 
+        assert(0 <= i and i < E.size());
+        assert(0 <= new_flow and new_flow <= new_cap);
+        auto& e = E[i];
+        auto& re = E[i^1];
+        e.cap = new_cap - new_flow; e.cost = new_cost;
+        re.cap = new_flow; re.cost = -new_cost;
     }
 };
