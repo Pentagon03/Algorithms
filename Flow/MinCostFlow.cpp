b89ad65e12b674e83f7012c9e596de56a4e0c813
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-21 17:28:54 +0900

    modify

--- Flow/MinCostFlow.cpp
+++ Flow/MinCostFlow.cpp
@@ -9,7 +9,7 @@
 - finds Augmenting path less or equal to 'paths_limit' times.
 - reference: hijkl2e, https://www.acmicpc.net/source/62155473
 ## Time Complexity
-- O(FEV)
+- O(min(F, VC) \cdot EV) (C is Cost Scale)
 - O(FE) on random data
 */
 // using Cap = int32_t; using Cost = int64_t;
@@ -65,6 +65,7 @@ struct mcf_graph {
 	static constexpr int paths_inf = numeric_limits<int>::max();
 	mcf_graph(int V) : n(V), G(V), d(V), last(V), vis(V){}
 	void add_edge(int u, int v, Cap cap, Cost cost) {
+		assert(cap > 0); // why do you need cap == 0?
 		assert(0 <= u and u < n and 0 <= v and v < n);
 		G[u].push_back(E.size());
 		E.push_back({v, cap, cost});
