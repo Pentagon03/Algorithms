f0fa434ee47999305373c451f34e6d049e558c41
Pentagon03 <kyungmoku7141@gmail.com>
2024-11-30 17:19:37 +0900

    modify

--- Data Structures/RangeQueries/RMQ.cpp
+++ Data Structures/RangeQueries/RMQ.cpp
@@ -1,26 +1,27 @@
 /*
 RMQ (range minimum query)
-usage: 
-vector<int> v;
-RMQ st(v, ranges::min);
+usage:
+vector<int> v = {1, 2, 3, 4, 5};
+RMQ st(v, ranges::min); // or make a custom lambda
 reference: https://github.com/kth-competitive-programming/kactl/blob/main/content/data-structures/RMQ.h
 */
 template<typename T, class Func = function<T(const T&, const T&)>>
 struct RMQ {
-	vector<vector<T>> jmp;
-	Func f;
-	RMQ(const vector<T>& V, Func g) : jmp(1, V), f(g) {
-		for (int pw = 1, k = 1; pw * 2 <= size(V); pw *= 2, ++k) {
-			jmp.emplace_back(size(V) - pw * 2 + 1);
-			for(int j=0;j<size(jmp[k]);j++)
-				jmp[k][j] = f(jmp[k - 1][j], jmp[k - 1][j + pw]);
-		}
-	}
-	T query(int a, int b) {
-		assert(a <= b); // or return inf if a > b
-		++b;
-		// __builtin_clz(1) - __builtin_clz(b - a);
-		int dep = 31 - __builtin_clz(b - a);
-		return f(jmp[dep][a], jmp[dep][b - (1 << dep)]);
-	}
+    vector<vector<T>> jmp;
+    Func f;
+    RMQ(){}
+    RMQ(const vector<T>& V, Func g) : jmp(1, V), f(g) {
+        for (int pw = 1, k = 1; pw * 2 <= size(V); pw *= 2, ++k) {
+            jmp.emplace_back(size(V) - pw * 2 + 1);
+            for(int j=0;j<size(jmp[k]);j++)
+                jmp[k][j] = f(jmp[k - 1][j], jmp[k - 1][j + pw]);
+        }
+    }
+    T query(int a, int b) {
+        assert(a <= b); // or return inf if a > b
+        ++b;
+        // __builtin_clz(1) - __builtin_clz(b - a);
+        int dep = __lg(b-a);
+        return f(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+    }
 };
--- Strings/Suffix_Array_Benq.cpp
+++ Strings/Suffix_Array_Benq.cpp
@@ -1,48 +1,52 @@
-// https://github.com/bqi343/cp-notebook/blob/master/Implementations/content/strings%20(14)/Light/SuffixArray%20(14.4).h
 /**
- * Description: Sort suffixes. First element of \texttt{sa} is \texttt{sz(S)},
- 	* \texttt{isa} is the inverse of \texttt{sa}, and \texttt{lcp} stores
- 	* the longest common prefix between every two consecutive elements of \texttt{sa}.
- * Time: O(N\log N)
- * Source: SuprDewd, KACTL, majk, ekzhang (http://ekzlib.herokuapp.com)
- * Verification: 
-	* https://open.kattis.com/problems/suffixsorting
-	* https://judge.yosupo.jp/problem/suffixarray
+ * Suffix Array & LCP Array
+ * sa[i]: index of ith shortest suffix
+ * sa[0]: sz(s) (because empty string is lexicographically first)
+ * isa[i]: s[i:] is isa[i]th fastest suffix, inverse of sa (isa[sa[i]]=i), 
+ * lcp[i]: longest common prefix between sa[i], sa[i+1]
+ *  **CONSIDER DELETING _chk IF WRONG ANSWER** (surface_03)
+ * reference: https://github.com/bqi343/cp-notebook/blob/master/Implementations/content/strings%20(14)/Light/SuffixArray%20(14.4).h
  */
-
-// sa: index of ith lexicographical fastest suffix
-// isa: inverse sa
-// lcp[i]: longest common prefix of sa[i] and sa[i+1]
+template<class str>
 struct SuffixArray {
-	str S; int N; vi sa, isa, lcp;
-	void init(str _S) { N = sz(S = _S)+1; genSa(); genLcp(); }
-	void genSa() { // sa has size sz(S)+1, starts with sz(S)
-		sa = isa = vi(N); sa[0] = N-1; iota(1+all(sa),0);
+	str S;
+	int N;
+	vector<int> sa, isa, lcp;
+    SuffixArray(){}
+	SuffixArray(const str& _S) { N = size(S = _S)+1; genSa(); genLcp(); }
+	void genSa() { // sa has size size(S)+1
+		sa = isa = vector<int>(N);
+		sa[0] = N-1;
+		iota(1+all(sa),0);
 		sort(1+all(sa),[&](int a, int b) { return S[a] < S[b]; });
-		FOR(i,1,N) { int a = sa[i-1], b = sa[i];
-			isa[b] = i > 1 && S[a] == S[b] ? isa[a] : i; }
+		for(int i=1;i<N;++i) {
+			int a = sa[i-1], b = sa[i];
+			isa[b] = i > 1 && S[a] == S[b] ? isa[a] : i; 
+		}
 		for (int len = 1; len < N; len *= 2) { // currently sorted
 			// by first len chars
-			vi s(sa), is(isa), pos(N); iota(all(pos),0); 
-			each(t,s) {int T=t-len;if (T>=0) sa[pos[isa[T]]++] = T;}
-			FOR(i,1,N) { int a = sa[i-1], b = sa[i]; /// verify that nothing goes out of bounds
-				isa[b] = is[a]==is[b]&&is[a+len]==is[b+len]?isa[a]:i; }
+			vector<int> s(sa), is(isa), pos(N); iota(all(pos),0);
+			for(auto &t: s) {
+				int T=t-len;
+				if (T>=0) sa[pos[isa[T]]++] = T;
+			}
+			bool _chk = false;
+			for(int i=1;i<N;++i) {
+				int a = sa[i-1], b = sa[i]; /// verify that nothing goes out of bounds
+				isa[b] = is[a]==is[b]&&is[a+len]==is[b+len]?isa[a]:i;
+				if(isa[b]!=i) _chk=true;
+			}
+			if(not _chk) break;
 		}
 	}
 	void genLcp() { // Kasai's Algo
-		lcp = vi(N-1); int h = 0;
-		F0R(b,N-1) { int a = sa[isa[b]-1];
-			while (a+h < sz(S) && S[a+h] == S[b+h]) ++h;
-			lcp[isa[b]-1] = h; if (h) h--; }
-		/// if we cut off first chars of two strings with lcp h
-		/// then remaining portions still have lcp h-1 
-		R.init(lcp); 
-	}
-	RMQ<int> R; 
-	int getLCP(int a, int b) { // lcp of suffixes starting at a,b
-		if (a == b) return sz(S)-a;
-		int l = isa[a], r = isa[b]; if (l > r) swap(l,r);
-		return R.query(l,r-1);
+		lcp = vector<int>(N-1);
+		int h = 0;
+		for(int b=0;b<N-1;++b) {
+			int a = sa[isa[b]-1];
+			while (a+h < size(S) && S[a+h] == S[b+h]) ++h;
+			lcp[isa[b]-1] = h;
+			if (h) h--;
+		}
 	}
 };
-
