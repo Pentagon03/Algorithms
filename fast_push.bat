0c53947835332f45efbff19820753a818b5f6c68
Pentagon03 <pentagon03.codes@gmail.com>
2024-12-10 02:18:06 +0900

    modify

--- fast_push.bat
+++ fast_push.bat
@@ -1,6 +1,18 @@
-call make_snippet.bat
+@echo off
+:: 커밋 메시지 인자 확인
+if "%~1"=="" (
+    set COMMIT_MESSAGE=modify
+) else (
+    set COMMIT_MESSAGE=%*
+)
+
+:: 커밋 메시지 출력 (echo를 통해 출력)
+echo Commit message: %COMMIT_MESSAGE%
+
+call _make_snippet.bat
+
+:: Git 명령어 실행 (이 부분은 echo off 상태로 진행)
 git pull
-git rm -r --cached .
 git add .
-git commit -m "modify"
+git commit -m "%COMMIT_MESSAGE%"
 git push
