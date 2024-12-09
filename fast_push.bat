e707cf70f6cf1ef182779a6fdec8fca8af4f7824
Pentagon03 <pentagon03.codes@gmail.com>
2024-12-10 02:18:36 +0900

    modify

--- fast_push.bat
+++ fast_push.bat
@@ -7,11 +7,11 @@ if "%~1"=="" (
 )
 
 :: 커밋 메시지 출력 (echo를 통해 출력)
-echo Commit message: %COMMIT_MESSAGE%
+echo Commit message: "%COMMIT_MESSAGE%"
 
 call _make_snippet.bat
 
-:: Git 명령어 실행 (이 부분은 echo off 상태로 진행)
+:: Git 명령어 실행
 git pull
 git add .
 git commit -m "%COMMIT_MESSAGE%"
