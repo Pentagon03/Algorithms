4a2b884c709f9692cec3e18f9f21ba996495dc71
Pentagon03 <pentagon03.codes@gmail.com>
2024-12-16 19:21:22 +0900

    Matrix

--- Math/[WIP] Matrix.cpp
+++ Math/[WIP] Matrix.cpp
@@ -1,30 +1,79 @@
-using matrix = vector<vector<int>>;
-matrix fail = {{-1}};
-void init(matrix&A,int r,int c){
-	A.resize(r);
-	for(int i=0;i<r;i++) A[i].resize(c);
-}
-const int MOD = 1e9+7;
-matrix operator*(const matrix&A,const matrix&B){
-	int r1=A.size(),c1=A[0].size(),r2=B.size(),c2=B[0].size();
-	if(c1!=r2) return fail;
-	T C; init(C,r1,c2);
-	for(int i=0;i<r1;i++)
-		for(int k=0;k<c1;k++){
-			const ll tmp = A[i][k];
-			for(int j=0;j<c2;j++){
-				C[i][j] += tmp*B[k][j]%MOD;
-				C[i][j] %= MOD;
-			}
-		}
-	return C;
-}
-matrix operator^(const matrix&A,ll x){
-	if(x<=0) return fail;
-	T1 ans = A; --x;
-	while(x>0){
-		if(x&1) ans = ans * A;
-		x>>=1; A = A*A;
-	}
-	return ans;
-}
+template<typename T>
+class Matrix {
+private:
+    vector<vector<T>> mat;
+    int rows, cols;
+
+public:
+    Matrix(int r, int c) : rows(r), cols(c) {
+        mat.resize(r, vector<T>(c, 0));
+    }
+
+    Matrix(const vector<vector<T>>& m) : mat(m) {
+        rows = m.size();
+        cols = m[0].size();
+    }
+
+    Matrix operator*(const Matrix& other) const {
+        assert(cols == other.rows);
+        Matrix result(rows, other.cols);
+        
+        // ikj 순서로 행렬 곱셈
+        for(int i = 0; i < rows; i++) {
+            for(int k = 0; k < cols; k++) {
+                auto val = mat[i][k];
+                if(val == 0) continue;
+                for(int j = 0; j < other.cols; j++) {
+                    result.mat[i][j] += val * other.mat[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    Matrix operator^(ll n) const {
+        assert(rows == cols and n >= 0);
+        Matrix result(rows, cols);
+        Matrix cur = *this;
+        for(int i=0;i<rows;i++) result[i][i] = 1;
+        while(n){
+            if(n&1) result = result * cur;
+            cur = cur * cur;
+            n >>= 1;
+        }
+        return result;
+    }
+
+    Matrix& operator+=(const Matrix& other) {
+        assert(rows == other.rows && cols == other.cols);
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                mat[i][j] += other.mat[i][j];
+            }
+        }
+        return *this;
+    }
+
+
+    Matrix operator+(const Matrix& other) const {
+        Matrix result = *this;
+        result += other;
+        return result;
+    }
+
+    vector<T>& operator[](int idx) {
+        return mat[idx];
+    }
+
+    int getRowsSize() const { return rows; }
+    int getColsSize() const { return cols; }
+
+    void print() const {
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                cout << mat[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+    }
+};
