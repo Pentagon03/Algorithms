783198446d640b2df7d278eda6d207222f843a7c
Pentagon03 <pentagon03.codes@gmail.com>
2024-12-22 04:17:45 +0900

    lis_recover

--- Adhoc/lis.cpp
+++ Adhoc/lis.cpp
@@ -8,3 +8,24 @@ int lis(vector<T> &v){
     }
     return t.size();
 }
+template<typename T = int, typename cmp = less<T>>
+vector<int> lis_recover(vector<T> &v){
+    int n = v.size();
+    vector<T> t(n);
+    vector<int> idx(n), before(n);
+    int ans = 0;
+    for(int i=0;i<n;i++){
+        int loc = lower_bound(begin(t), begin(t) + ans, v[i], cmp()) - t.begin();
+        if(loc == ans) ++ans;
+        t[loc] = v[i]; idx[loc] = i;
+        before[i] = loc ? idx[loc - 1] : -1;
+    }
+    vector<int> res;
+    int cur = idx[ans - 1];
+    while(cur != -1){
+        res.push_back(v[cur]);
+        cur = before[cur];
+    }
+    reverse(all(res));
+    return res;
+}
