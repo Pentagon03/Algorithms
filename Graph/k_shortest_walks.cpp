88970005411a0685b523f6e7271e30f50e365469
Kyungmo Ku <pentagon03.codes@gmail.com>
2025-01-06 14:04:16 +0900

    Update k_shortest_walks.cpp

--- Graph/k_shortest_walks.cpp
+++ Graph/k_shortest_walks.cpp
@@ -121,11 +121,11 @@ public:
         for(int64_t u = 0; u < n_; u++)
             if(not in_deg[u])
                 q.push(u);
-        std::vector<int64_t> order(n_);
-        for(auto &u : order){
+        std::vector<int64_t> sorted_vertices(n_);
+        for(auto &u : sorted_vertices){
             if(q.empty()) {
                 assert(0 && "Cycle occured in DAG");
-                return order;
+                return sorted_vertices;
             }
             u = q.front();
             q.pop();
@@ -133,7 +133,7 @@ public:
                 if(--in_deg[v] == 0)
                     q.push(v);
         }
-        return std::move(order);
+        return std::move(sorted_vertices);
     }
 
     // O(|E| + |V|), Solves DAG - shortest path problem
@@ -142,8 +142,8 @@ public:
         std::vector<Distance> d_(g_.size(), MAX_DISTANCE);
         std::vector<int64_t> prv(g_.size(), -1);
         d_[s] = IDENTITY_DISTANCE;
-        auto order = topology_sort(g_);
-        for(auto v: order){
+        auto sorted_vertices = topology_sort(g_);
+        for(auto v: sorted_vertices){
             if(d_[v] == MAX_DISTANCE) continue;
             for(auto &[to, w] : g_[v]){
                 if(d_[to] > d_[v] + w){
